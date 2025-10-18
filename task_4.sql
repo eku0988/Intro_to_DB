feb3e922d5af84a030fa3c5bddcce030c8074e77
Ekram Desta <ekramdesta0988@gmail.com>
2025-10-18 17:36:24 +0300

    full description of books table, keywords uppercase, dynamic DB

--- task_4.sql
+++ task_4.sql
@@ -1,17 +1,4 @@
 -- task_4.sql
--- Print full description of the table 'books' without using DESCRIBE, EXPLAIN, or ANALYZE
+-- Full description of the table 'books' without DESCRIBE, EXPLAIN, ANALYZE, or INFORMATION_SCHEMA
 
-SELECT 
-    COLUMN_NAME AS `Column`,
-    COLUMN_TYPE AS `Type`,
-    IS_NULLABLE AS `Nullable`,
-    COLUMN_KEY AS `Key`,
-    COLUMN_DEFAULT AS `Default`,
-    EXTRA AS `Extra`
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_SCHEMA = DATABASE()  -- Use the database passed to mysql command
-    AND TABLE_NAME = 'books'
-ORDER BY 
-    ORDINAL_POSITION;
+SHOW CREATE TABLE books;
