222bcd221c1b534ff59c78480f0877f4519f0253
Ekram Desta <ekramdesta0988@gmail.com>
2025-10-19 22:13:33 +0300

    Fix task_4.sql: display full description of books table without DESCRIBE or ANALYZE

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,16 @@
 -- Script: task_4.sql
--- Prints the full description (table definition) of the table Books
+-- Prints the full description of the table books
 -- Database name will be passed as argument
 
-SHOW CREATE TABLE books;
+SELECT 
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    information_schema.columns
+WHERE 
+    table_schema = DATABASE()
+    AND table_name = 'books';
