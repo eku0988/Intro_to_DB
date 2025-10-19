6931ff42a4d397eb5592e74dbfa85c74eceef196
Ekram Desta <ekramdesta0988@gmail.com>
2025-10-19 22:07:56 +0300

    Add task_4.sql script to display full description of books table

--- task_4.sql
+++ task_4.sql
@@ -1,16 +1,5 @@
 -- Script: task_4.sql
--- Prints the full description of the table books
+-- Prints the full description (table definition) of the table Books
 -- Database name will be passed as argument
 
-SELECT 
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_SCHEMA = DATABASE()
-    AND TABLE_NAME = 'books';
+SHOW CREATE TABLE Books;
