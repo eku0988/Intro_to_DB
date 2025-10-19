c5c3668f867aa7e99e3ba5f98cb854a974cdbd24
Ekram Desta <ekramdesta0988@gmail.com>
2025-10-19 07:34:54 +0300

    Add task_4.sql to print full description of books table

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,15 @@
 -- task_4.sql
 -- Print the full description of the table 'books'
 
-SHOW FULL COLUMNS FROM books;
+SELECT 
+    COLUMN_NAME AS 'Column',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'books';
